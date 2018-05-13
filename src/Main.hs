bfc54309483a5a852f93e17ff9f79e72d17708fc
rexim <reximkut@gmail.com>
2018-05-13 10:10:08 +0700

    (#3) Add output folder as an argument to poutro CLI

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Outro
 import           System.Environment
+import           Text.Printf
 
 frameFileNames :: String -> [String]
 frameFileNames prefix = map (\n -> prefix ++ show n ++ ".svg") [1 .. ]
@@ -9,14 +10,15 @@ frameFileNames prefix = map (\n -> prefix ++ show n ++ ".svg") [1 .. ]
 loadNamesFromFile :: FilePath -> IO [String]
 loadNamesFromFile fileName = lines <$> readFile fileName
 
+-- TODO: poutro doesn't create the output folder if it doesn't exist
 mainWithArgs :: [String] -> IO ()
-mainWithArgs [namesFileName] = do
+mainWithArgs (namesFileName:outputFolder:_) = do
   names <- loadNamesFromFile namesFileName
   sequence_
     $ map (uncurry saveFrame)
-    $ zip (frameFileNames "frame-")
+    $ zip (frameFileNames $ printf "%s/" outputFolder)
     $ outroFromNames defaultDisplay names
-mainWithArgs _ = error "Usage: ./poutro <names-file>"
+mainWithArgs _ = error "Usage: ./poutro <names-file> <output-folder>"
 
 main :: IO ()
 main = getArgs >>= mainWithArgs
