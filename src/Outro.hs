c520b5cb9a747489ccdb3d1a16caac84e26354c6
rexim <reximkut@gmail.com>
2018-05-19 18:22:45 +0700

    Improve animation

--- src/Outro.hs
+++ src/Outro.hs
@@ -34,7 +34,11 @@ instance Fractional V2 where
 timehopEvents :: [Double]
 timehopEvents = map (* (1.0 / 30.0)) [1, 22, 30, 60, 72, 83, 90, 121, 143, 150, 180, 192, 203, 210, 241]
 
-nameSlots = map ((V2 (-400) &&& V2 780) . (+ 270) . (* 75)) [0 .. 8]
+nameSlots :: [(V2, V2)]
+nameSlots = map (\(x, y) -> ( V2 x (y * 75 + 270)
+                            , V2 780 (y * 75 + 270)
+                            ))
+              $ zip (cycle [-400, 2000]) [0 .. 8]
 
 defaultDisplay :: Display
 defaultDisplay = Display { displayWidth = 1920
@@ -126,7 +130,7 @@ outroFromNames display names =
                  ])
       $ zip timehopEvents elements
     where fps = fromIntegral $ displayFps display
-          elements = [(supportedBy, (V2 0 150, V2 660 150))]
+          elements = [(supportedBy, (V2 660 0, V2 660 150))]
                        ++ zip (map (`textElement` 60) names) nameSlots
                        ++ [ (textElement "via" 100, (V2 (-450) 1000, V2 750 1000))
                           , (patreonLogo, (V2 1920 925, V2 950 925))]
