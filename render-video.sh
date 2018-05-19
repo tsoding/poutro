d5117b40b5e5dc524436fff07a30ffe768398c96
rexim <reximkut@gmail.com>
2018-05-19 14:03:55 +0700

    Render video with 30 fps

--- render-video.sh
+++ render-video.sh
@@ -6,5 +6,5 @@ set -xe
 
 pushd out/
   find -type f -name \*.svg | xargs -t -P 5 -n 1 -I xx convert xx xx.png
-  ffmpeg -framerate 60 -i %d.svg.png output.mp4
+  ffmpeg -framerate 30 -i %d.svg.png output.mp4
 popd
