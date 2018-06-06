3a4e611c5ff79158417398d0441ec6c901a27291
rexim <reximkut@gmail.com>
2018-06-06 17:25:12 +0700

    bouncyAppear animation in terms of Num and Fractional

--- src/Animations.hs
+++ src/Animations.hs
@@ -9,7 +9,6 @@ module Animations where
 import           Data.List
 import           Text.Blaze.Svg (toSvg)
 import qualified Text.Blaze.Svg11 as S
-import           V2
 
 animate :: (Fractional a, Num a) => (a, a) -> Double -> Double -> (a -> S.Svg) -> [S.Svg]
 animate (startPos, endPos) fps duration f = map (\i -> f (startPos + dv * fromInteger i)) [0 .. n]
@@ -19,14 +18,14 @@ animate (startPos, endPos) fps duration f = map (\i -> f (startPos + dv * fromIn
 
 waitFor :: S.Svg -> Double -> Double -> [S.Svg]
 waitFor object fps duration = map (const object) [0 .. n]
-    where dt = 1.0/ fps
+    where dt = 1.0 / fps
           n = duration / dt
 
-bouncyAppear :: (V2 Double, V2 Double) -> Double -> (V2 Double -> S.Svg) -> [S.Svg]
+bouncyAppear :: (Fractional a, Num a) => (a, a) -> Double -> (a -> S.Svg) -> [S.Svg]
 bouncyAppear (start, end) fps f =
     animate (start, overshoot) fps 0.1 f
       ++ animate (overshoot, end) fps 0.1 f
-    where overshoot = end + (end - start) * V2 0.15 0.15
+    where overshoot = end + (end - start) * fromRational 0.15
 
 parallelCombine :: [[S.Svg]] -> [S.Svg]
 parallelCombine = map toSvg . transpose
