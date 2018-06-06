a5619a56af54fcda8db49538a1e19a5d948c4f42
rexim <reximkut@gmail.com>
2018-06-06 16:21:36 +0700

    Introduce BetweenTwoSets video

---
+++ src/Videos/BetweenTwoSets.hs
@@ -0,0 +1,13 @@
+{-|
+Module : BetweenTwoSets
+
+Animations for HaskellRank episode about solving
+https://www.hackerrank.com/challenges/between-two-sets/problem
+-}
+
+module BetweenTwoSets where
+
+import           Frame
+
+rules :: [Frame]
+rules = undefined
