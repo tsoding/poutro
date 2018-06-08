90a64336a4ed42919b65a14a50525def210639d0
rexim <reximkut@gmail.com>
2018-06-08 19:43:54 +0700

    More flexible name slots layout

--- src/Videos/Outro.hs
+++ src/Videos/Outro.hs
@@ -10,17 +10,26 @@ import           Animations
 import           Display
 import           Elements
 import           Frame
+import           Layouts
 import           V2
 
 timehopEvents :: [Double]
 timehopEvents = map (* (1.0 / 30.0)) [1, 22, 30, 60, 72, 83, 90, 121, 143, 150, 180, 192, 203, 210, 241]
 
-nameSlots :: [(V2 Double, V2 Double)]
-nameSlots = map (\(x, y) -> ( V2 x (y * 75 + 270)
-                            , V2 780 (y * 75 + 270)
-                            ))
-              $ zip (cycle [-400, 2000]) [0 .. 8]
-
+nameSlots :: Display            -- display
+          -> [(V2 Double, V2 Double)]
+nameSlots display = concat [ zip (verticalLineLayout display rows spacing leftOffscreen)
+                                 (verticalLineLayout display rows spacing leftColumn)
+                           , zip (verticalLineLayout display rows spacing rightOffscreen)
+                                 (verticalLineLayout display rows spacing rightColumn)
+                           ]
+    where rows = 6
+          spacing = 100.0
+          leftOffscreen = -500
+          rightOffscreen = width
+          leftColumn = 400.0
+          rightColumn = width - 600.0
+          width = fromIntegral $ displayWidth display
 
 outro :: Display -> [String] -> [Frame]
 outro display names =
@@ -34,7 +43,7 @@ outro display names =
       $ zip timehopEvents elements
     where fps = fromIntegral $ displayFps display
           elements = [(supportedBy, (V2 660 0, V2 660 150))]
-                       ++ zip (map (`textElement` 60) names) nameSlots
+                       ++ zip (map (`textElement` 60) names) (nameSlots display)
                        ++ [ (textElement "via" 100, (V2 (-450) 1000, V2 750 1000))
                           , (patreonLogo, (V2 1920 925, V2 950 925))]
           duration = 8.5
